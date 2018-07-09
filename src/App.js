0d7f020d576d6f7737619c57a8c18c1d297597d0
Christopher Vicuña <christopher.vicuna@gmail.com>
2018-07-09 12:14:33 -0400

    adding my first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import WeatherLocation from './Components/WeatherLocation'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hola mundo
+        <WeatherLocation></WeatherLocation>
       </div>
     );
   }
